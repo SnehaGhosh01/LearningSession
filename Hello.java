4abf5e47f84567fcf1fbaf42cf5e881e62730e5b
SnehaGhosh01 <sneha.ghosh.uem@gmail.com>
2023-12-03 13:02:01 +0530

    HelloWorlD SNEHA 3

--- Hello.java
+++ Hello.java
@@ -2,4 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World 3");
     }
+    public void hello() {
+        System.out.println("Hello World Sneha");
+    }
 }
